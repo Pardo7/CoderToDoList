4a52fd7b1292a8c1de2c20dee150b84a736f7edc
Pardo Walter <walterpardo7@gmail.com>
2015-02-10 14:16:18 -0800

    first commit

---
+++ readMe.md
@@ -0,0 +1,8 @@
+# A Coders To Do List
+
+![Smaller icon](http://www.shirts4geek.com/blog/wp-content/uploads/2014/01/coder-boy456789.jpg)
+
+
+## Overview 
+
+**A coders to do list**, The worlds most simple to do list for programmers.
---
+++ simple-todos.js
@@ -0,0 +1,38 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+ // This code only runs on the client
+ Template.body.helpers({
+     tasks: function () {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+     }
+ });
+
+ Template.body.events({
+     "submit .new-task": function (event) {
+      // Im calling this function when a new task form is submitted
+         var text = event.target.text.value;
+
+         Tasks.insert({
+             text: text,
+             createdAt: new Date() // current time
+         });
+
+         //Clear Form
+         event.target.text.value = "";
+
+         // prevent default form submit
+         return false;
+     }
+ });
+
+Template.task.events({
+    "click .toggle-checked": function () {
+    // Here I'll set the checked property to the opposite of its current value
+     Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+        Tasks.remove(this._id);
+    }
+});
+}
